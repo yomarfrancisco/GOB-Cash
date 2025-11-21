37bd547c20d6c5ae989ec3f71422ac8a7c630de2
yomarfrancisco <ygor.francisco@gmail.com>
2025-11-21 14:12:17 +0200

    Replace GoBankless logo with The_firm.png in TopGlassBar
    - Update logo path from /assets/core/gobankless-logo.png to /assets/The_firm.png
    - Add new logo asset The_firm.png to public/assets/

--- src/components/TopGlassBar.tsx
+++ src/components/TopGlassBar.tsx
@@ -48,7 +48,7 @@ export default function TopGlassBar({ onScanClick }: TopGlassBarProps = {}) {
         />
       ) : (
         <Image
-          src="/assets/core/gobankless-logo.png"
+          src="/assets/The_firm.png"
           alt="GoBankless"
           className="gobankless-logo"
           width={220}
